7f477e3e564163c5320ff7519d2cee235efe3e85
mehedihasan <ehasanmunn84@gmail.com>
2024-01-23 08:21:22 +0600

    ✅ create global error handler middleware

--- app/middleware/globalErrorHandler.js
+++ app/middleware/globalErrorHandler.js
@@ -1,26 +1,31 @@
 /* eslint-disable no-unused-vars */
 const golbalErrorsHandler = (error, req, res, next) => {
   // eslint-disable-next-line no-console
-// console.log(error.name);
+  // console.log(error.name);
   let statusCode = 500;
-
-  let errorMessages = [];
+  let message = "";
+  let errorMessages = "";
 
   if (error.name === "ValidationError") {
-    errorMessages = Object.values(error.errors).map((val) => val.message);
+    message=error?.name
+    errorMessages = error?.message;
+    
     // console.log(errorMessages);
-  }
-  else if (error.name === "MongoServerError") {
-    statusCode=(error.code);
-    errorMessages.push(error.message)
+  } else if (error.name === "MongoServerError") {
+    statusCode = error.code;
+    message=error.name
+    errorMessages = error.message;
+  } else if (error instanceof Error) {
+    message=error?.name
+    errorMessages = error?.message;
   }
   // eslint-disable-next-line no-console
   console.log("❌ Error:", errorMessages);
   res.json({
     status: false,
     code: statusCode,
-    message: "something went wrong",
-    errorMessage: errorMessages
+    message,
+    errorMessage: errorMessages,
   });
 };
 export default golbalErrorsHandler;
